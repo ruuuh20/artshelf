6537fd9599d168659765843aa7112a223216760b
ruuuh20 <pkim2320@gmail.com>
2020-12-21 11:18:44 -0500

    ignore embed file please

--- pages/embed/[...library].js
+++ pages/embed/[...library].js
@@ -1,10 +1,10 @@
-// import { useRef, useState } from 'react';
-// import { useRouter } from 'next/router';
-// import { Box, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
-// import Book from '@/components/Book';
-// import { useAuth } from '@/lib/auth';
-// import { createBook} from '@/lib/db';
-// import { getAllBooks, getAllLibraries } from '@/lib/db-admin';
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Box, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import Book from '@/components/Book';
+import { useAuth } from '@/lib/auth';
+import { createBook} from '@/lib/db';
+import { getAllBooks, getAllLibraries } from '@/lib/db-admin';
 
 // export async function getStaticProps(context) {
 //   const libraryId = context.params.libraryId;
@@ -32,54 +32,54 @@
 //   };
 // }
 
-// const BooksPage = ({ initialBooks }) => {
-//   const auth = useAuth();
-//   const router = useRouter();
-//   const inputEl = useRef(null);
-//   const [allBooks, setAllBooks] = useState(initialBooks);
+const BooksPage = ({ initialBooks }) => {
+  // const auth = useAuth();
+  // const router = useRouter();
+  // const inputEl = useRef(null);
+  // const [allBooks, setAllBooks] = useState(initialBooks);
 
-//   const onSubmit = (e) => {
-//     e.preventDefault();
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
 
-//     const newBook= {
-//       author: auth.user.name,
-//       authorId: auth.user.uid,
-//       libraryId: router.query.libraryId,
-//       description: inputEl.current.value,
-//       createdAt: new Date().toISOString(),
-//       provider: auth.user.provider,
-//       status: 'pending'
-//     };
-//     inputEl.current.value = '';
-//     setAllBooks([newBook, ...allBooks]);
-//     createBook(newBook);
-//   };
+  //   const newBook= {
+  //     author: auth.user.name,
+  //     authorId: auth.user.uid,
+  //     libraryId: router.query.libraryId,
+  //     description: inputEl.current.value,
+  //     createdAt: new Date().toISOString(),
+  //     provider: auth.user.provider,
+  //     status: 'pending'
+  //   };
+  //   inputEl.current.value = '';
+  //   setAllBooks([newBook, ...allBooks]);
+  //   createBook(newBook);
+  // };
 
-//   return (
+  return (
+  <div></div>
+    // <Box
+    //   display="flex"
+    //   flexDirection="column"
+    //   width="full"
   
-//     <Box
-//       display="flex"
-//       flexDirection="column"
-//       width="full"
-  
-//     >
-//       {auth.user && (
-//         <Box as="form" onSubmit={onSubmit}>
-//          <FormControl my={8}>
-//             <FormLabel htmlFor="comment">Comment</FormLabel>
-//             <Input ref={inputEl} id="comment" placeholder="Leave a comment" />
-//             <Button mt={4} type="submit" fontWeight="medium" isDisabled={router.isFallback}>
-//               Add Comment
-//             </Button>
-//           </FormControl>
-//         </Box>
-//       )}
-//       {allBooks.map((book) => (
-//         <Book key={book.id} {...book} />
-//       ))}
-//     </Box>
+    // >
+    //   {auth.user && (
+    //     <Box as="form" onSubmit={onSubmit}>
+    //      <FormControl my={8}>
+    //         <FormLabel htmlFor="comment">Comment</FormLabel>
+    //         <Input ref={inputEl} id="comment" placeholder="Leave a comment" />
+    //         <Button mt={4} type="submit" fontWeight="medium" isDisabled={router.isFallback}>
+    //           Add Comment
+    //         </Button>
+    //       </FormControl>
+    //     </Box>
+    //   )}
+    //   {allBooks.map((book) => (
+    //     <Book key={book.id} {...book} />
+    //   ))}
+    // </Box>
  
-//   );
-// };
+  );
+};
 
-// export default BooksPage;
+export default BooksPage;
