015e70970eff04820588453110f410662859b919
ruuuh20 <pkim2320@gmail.com>
2020-12-21 15:35:08 -0500

    change books page to swr i think its working

--- pages/library/[...library].js
+++ pages/library/[...library].js
@@ -1,59 +1,76 @@
 import { useRef, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { Box, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Button, Textarea} from '@chakra-ui/react';
 import Book from '@/components/Book';
 import DashboardShell from '@/components/DashboardShell';
 import { useAuth } from '@/lib/auth';
 import { createBook} from '@/lib/db';
-import { getAllBooks, getAllLibraries, getLibrary } from '@/lib/db-admin';
+// import { getAllBooks, getAllLibraries, getLibrary } from '@/lib/db-admin';
 import LibraryHeader from '@/components/LibraryHeader'
 
-export async function getStaticProps(context) {
-  const [libraryId, route] = context.params.library;
-  const { books } = await getAllBooks(libraryId, route);
-  const { library } = await getLibrary(libraryId);
-  return {
-    props: {
-      initialBooks: books,
-      library
-    },
-    revalidate: 1
-  };
-}
+import useSWR, { mutate } from 'swr';
+import fetcher from '@/utils/fetcher';
 
-export async function getStaticPaths() {
-  const { libraries } = await getAllLibraries();
-  const paths = libraries.map((library) => ({
-    params: {
+// export async function getStaticProps(context) {
+//   const [libraryId, route] = context.params.library;
+//   const { books } = await getAllBooks(libraryId, route);
+//   const { library } = await getLibrary(libraryId);
+//   return {
+//     props: {
+//       initialBooks: books,
+//       library
+//     },
+//     revalidate: 1
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const { libraries } = await getAllLibraries();
+//   const paths = libraries.map((library) => ({
+//     params: {
       
-      library: [library.id.toString()]
-      // library: ['hello']
-    }
-  }));
-  console.log(paths[1])
+//       library: [library.id.toString()]
+//       // library: ['hello']
+//     }
+//   }));
+//   console.log(paths[1])
 
-  return {
-    paths,
-    fallback: true
-  };
-}
+//   return {
+//     paths,
+//     fallback: true
+//   };
+// }
 
 
-const BooksPage = ({ initialBooks, library }) => {
+// const BooksPage = ({ initialBooks, library }) => {
+const BooksPage = () => {
    const { user, loading } = useAuth();
   const router = useRouter();
   const inputEl = useRef(null);
-  const [allBooks, setAllBooks] = useState(initialBooks);
-  const [libraryId, route] = router.query.library;
+  // const [allBooks, setAllBooks] = useState(initialBooks);
+  // const [libraryId, route] = router.query.library;
+  const libraryAndRoute = router.query?.library;
+  const libraryId = libraryAndRoute ? libraryAndRoute[0] : null;
+  const route = libraryAndRoute ? libraryAndRoute[1] : null;
+  const booksApi = route
+    ? `/api/books/${libraryId}/${route}`
+    : `/api/books/${libraryId}`;
 
-  useEffect(() => {
-    setAllBooks(initialBooks);
-  }, [initialBooks]);
+  // useEffect(() => {
+  //   setAllBooks(initialBooks);
+  // }, [initialBooks]);
+
+  const { data: libraryData } = useSWR(`/api/library/${libraryId}`, fetcher);
+  const { data: booksData } = useSWR(booksApi, fetcher);
+
+  const library = libraryData?.library;
+  const allBooks = booksData?.books;
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const newBook= {
+      libraryAuthorId: library.authorId,
       libraryId,
       route: route || '/',
       author: user.name,
@@ -65,14 +82,45 @@ const BooksPage = ({ initialBooks, library }) => {
       status: 'pending'
     };
     inputEl.current.value = '';
-    setAllBooks([newBook, ...allBooks]);
+    // setAllBooks([newBook, ...allBooks]);
     createBook(newBook);
+    mutate(
+      booksApi,
+      async (data) => ({
+        books: [newBook, ...data.books]
+      }),
+      false
+    );
   };
 
+  const LoginOrLeaveBook = () =>
+    user ? (
+      <Button
+        type="submit"
+        isDisabled={!libraryData || !booksData}
+        backgroundColor="gray.900"
+        color="white"
+        fontWeight="medium"
+        mt={4}
+        _hover={{ bg: 'gray.700' }}
+        _active={{
+          bg: 'gray.800',
+          transform: 'scale(0.95)'
+        }}
+      >
+        Leave Feedback
+      </Button>
+    ) : (
+      // <LoginButtons />
+      <div>hi</div>
+    );
+
+      console.log(booksData)
   return (
     <DashboardShell>
       <LibraryHeader 
-      isLibraryOwner={true}
+      // isLibraryOwner={true}
+      isLibraryOwner={library?.authorId === user?.uid}
       library={library} 
       libraryId={libraryId} 
       route={route}
@@ -87,11 +135,15 @@ const BooksPage = ({ initialBooks, library }) => {
       
         <Box as="form" onSubmit={onSubmit}>
          <FormControl my={8}>
-            <FormLabel htmlFor="comment">Comment</FormLabel>
-            <Input ref={inputEl} id="comment" placeholder="Leave a comment" />
-            <Button mt={4} type="submit" fontWeight="medium" isDisabled={router.isFallback}>
-              Add Comment
-            </Button>
+           <Textarea
+              ref={inputEl}
+              id="comment"
+              placeholder="Leave a comment"
+              isDisabled={!user}
+              h="100px"
+            />
+            {!loading && <LoginOrLeaveBook />}
+            
           </FormControl>
         </Box>
      
