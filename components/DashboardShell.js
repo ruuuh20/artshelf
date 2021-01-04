915b36988d39aa605db3a59fec121c38ef34aa40
ruuuh20 <pkim2320@gmail.com>
2021-01-04 15:47:34 -0500

    fix images

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -14,6 +14,36 @@ import Footer from './Footer'
 
 import { useAuth } from '@/lib/auth';
 
+export const Logo = () => (
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.87 21.94">
+  <g id="Layer_2" data-name="Layer 2">
+    <g id="Layer_1-2" data-name="Layer 1">
+      <text
+        transform="translate(0 11.29)"
+        fontSize="{13}"
+        fill="#231f20"
+        fontFamily="AktivGrotesk-Hair, Aktiv Grotesk"
+        letterSpacing="-0.03em"
+      >
+        a
+        <tspan x="6.2" y="{0}" letterSpacing="-0.01em">r</tspan>
+        <tspan x="10.37" y="{0}" letterSpacing="-0.03em">t</tspan>
+      </text>
+      <text
+        transform="translate(14.78 11.14)"
+        fontSize="{13}"
+        fill="#231f20"
+        fontFamily="AktivGrotesk-Regular, Aktiv Grotesk"
+        letterSpacing="-0.06em"
+      >
+        r
+        <tspan x="3.93" y="{0}" letterSpacing="-0.03em">eads</tspan>
+      </text>
+    </g>
+  </g>
+</svg>
+)
+
 const DashboardShell = ({ children }) => {
   const { user, signout } = useAuth();
 
@@ -36,7 +66,7 @@ const DashboardShell = ({ children }) => {
         >
           <Flex>
             <NextLink href="/" passHref>
-            <Image src='./logo.svg' alt='logo' />
+            <Logo />
             </NextLink>
              <NextLink href="/dashboard" passHref>
             <Link mr={4}>Libraries</Link>
@@ -54,10 +84,10 @@ const DashboardShell = ({ children }) => {
             <Avatar size="sm" src={user?.photoUrl} />
           </Flex>
         </Flex>
-        <Center h="100px">
-              <Image src="/hero.jpg" width="100%" />
+        <Box>
+              <Image src="/hero.jpg" width="100%" height="350px" />
 
-        </Center>
+        </Box>
       </Flex>
       <Flex margin="0 auto" direction="column" maxW="1250px" px={8} bg="#fff">
         
