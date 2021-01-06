28b6704abd58545151ae3ec5f7e9b4cdb85deb98
ruuuh20 <pkim2320@gmail.com>
2021-01-06 18:36:53 -0500

    add web font

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
          <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500&display=swap" rel="stylesheet" />
+<link rel="stylesheet" href="https://use.typekit.net/ifo8cql.css"></link>
         </Head>
         <body>
           <Main />
